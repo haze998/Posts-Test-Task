8d1ccf8a47ef6b1dd3e5916390e4e364a14125f2
haze998 <docenko6996@gmail.com>
2023-08-30 13:32:25 +0300

    -create-project

---
+++ PostsTestApp/PostsTestApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PostsTestApp
+//
+//  Created by Evgeniy Docenko on 30.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
